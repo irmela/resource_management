d0fd4a5011fa4cf931ff2528cb716fbe9215f44d
Irmela Goehl <i.goehl@gmx.de>
2014-09-28 18:54:53 +0200

    Added basic validation

--- app/models/ressource.rb
+++ app/models/ressource.rb
@@ -1,6 +1,10 @@
 class Ressource < ActiveRecord::Base
 	has_many :jobs
 
+	validates :name, presence: true
+	validates :department, presence: true
+	validates :color, presence: true
+
 	def name_and_id
     	"#{self.name} (#{self.id})"
   	end
