3d72f7552936ae0f6c208d30630f322a3d8a0962
Irmela Goehl <i.goehl@gmx.de>
2014-09-28 18:16:58 +0200

    first steps of implementing pre-population

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -38,9 +38,8 @@ module CalendarHelper
         # remove Sat and Sun from hash
         week.delete(week.last)
         week.delete(week.last)
-        #concat link_to 'new', new_job_path
-        newLink = link_to '', new_job_path
     	results = week.map do |day|
+            newLink = link_to '', new_job_path({:ressource_id => ressourceID, :date => day})
         	content_tag( :div, jobs_by_date(day, ressourceID).to_s + newLink.to_s, :class => "day", :id => day)
     	end
         safe_join results
