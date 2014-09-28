2d0facd660f39efa70862724dc60ae6817155ab7
Irmela Goehl <i.goehl@gmx.de>
2014-09-28 10:40:49 +0200

    jobs by date and resource

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,36 +1,54 @@
 module CalendarHelper
 
-	def render_weeks
-        @date_range.each_slice(7).each do |week|
-        	label = content_tag( :span, "KW " + week[1].cweek.to_s)
-			week = content_tag( :ul, render_week(week), :class => "week")
-			concat content_tag( :div, label.to_s + week.to_s, :class => "week-container")
+	def render_table
+		content_tag :table, :class => "table" do
+        	concat content_tag( :thead, render_table_head)
+			concat content_tag( :tbody, render_ressources)
+		end
+    end
+
+    def render_table_head
+		content_tag :tr do
+			concat content_tag( :th, "Ressource")
+			results = @date_range.each_slice(7).map do |week|
+				concat content_tag( :th, "KW " + week[1].cweek.to_s)
+	        end
+	        safe_join results
+	    end
+    end
+
+    def render_ressources
+    	results = Ressource.all.map do |ressource|
+    		render_weeks(ressource.name, ressource.id)
+    	end
+    	safe_join results
+    end
+
+	def render_weeks(ressource, ressourceID)
+        content_tag :tr do
+            concat content_tag( :td, ressource)
+            results = @date_range.each_slice(7).map do |week|
+    			concat content_tag( :td, render_week(week, ressourceID), :class => "week")
+            end
+            safe_join results
         end
     end
 
-    def render_week(week)
+    def render_week(week, ressourceID)
     	results = week.map do |day|
-        	content_tag( :li, jobs_by_date(day), :class => "day", :id => day)
+        	content_tag( :div, jobs_by_date(day, ressourceID), :class => "day", :id => day)
     	end
         safe_join results
     end
 
-    def jobs_by_date(date)
-    	# results = week.map do |day|
-     #    	content_tag( :li, day.day, :class => "day", :id => day)
-    	# end
-     #    safe_join results
-
-     	# return date.day
-     	return Job.first.project_id if date.between?(Job.first.start_date, Job.first.end_date)
+    def jobs_by_date(date, ressourceID)
+	    results = Job.all.map do |job|
+	     	if date.between?(job.start_date, job.end_date) && ressourceID == job.ressource_id
+	     		projectID = job.project_id
+	     		projectColor = Project.find_by_id(projectID).color
+	     		content_tag( :div, date.day, :style => "background: #{projectColor};")
+	    	end
+	    end
+	    safe_join results
     end
-
-    # def render_week(week)
-    #   results = week.map do |day|
-    #     content_tag :td, get_option(:td, start_date, day) do
-    #       #block.call(day, events_for_date(day))
-    #     end
-    #   end
-    #   safe_join results
-    # end
 end
