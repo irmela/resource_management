1521c1a61ca3a09b9c864cd08f27ecc62fcaff12
Irmela Goehl <i.goehl@gmx.de>
2014-09-28 10:41:00 +0200

    some cleanup

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -7,26 +7,4 @@ class CalendarController < ApplicationController
 		@date_range = (@start_date..@week_end)
     @ressources = Ressource.all
 	end
-	# def render(block)
- #      @block = block
-
- #      capture do
- #        concat render_weeks
- #      end
- #    end
-
-	# def render_weeks
- #      capture do
- #        @date_range.each_slice(7) do |week|
- #          concat content_tag( :ul, week)
- #        end
- #      end
- #    end
-
-    # def render_week(week)
-    # 	results = week.map do |day|
-    #     	content_tag ( :li, day)
-    # 	end
-    #     safe_join results
-    # end
 end
