85ddef68f862181bf02263fb849ea1b58257a1c7
Irmela Goehl <i.goehl@gmx.de>
2014-11-04 22:00:28 +0100

    structure tweaking

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -3,6 +3,7 @@ class Calendar
 	def initialize(params = {})
 		@week_start = params[:cal_start] ? DateTime.parse(params[:cal_start]).beginning_of_week : Date.today.beginning_of_week
   		@week_end = @week_start + 41
+  		$ressources = params[:department] ? Ressource.where(department: params[:department]) : Ressource.all.order('department')
 	end
 
 	def weeks
