1a3297e8c9fa6f498fc1bd3f30fc4d533f3c5874
Irmela Goehl <i.goehl@gmx.de>
2014-11-10 22:28:45 +0100

    finally got rid of the global variables and did some minor cleanup

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,6 +1,6 @@
 class CalendarsController < ApplicationController
   def show
-  	$calendar = Calendar.new(params)
-  	$ressources = params[:department] ? Ressource.where(department: params[:department]) : Ressource.all.order('department')
+  	@calendar = Calendar.new(params)
+  	@ressources = params[:department] ? Ressource.where(department: params[:department]) : Ressource.all.order('department')
   end
 end
