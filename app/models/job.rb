2a8cf918e735b74d59ff54fb47c4e3b51952e1df
Irmela Goehl <irmela@Irmelas-Air.fritz.box>
2015-01-14 21:15:31 +0100

    replaced custom tooltip solution with tipsy library

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,4 +6,12 @@ class Job < ActiveRecord::Base
 	validates :ressource_id, presence: true
 	validates :start_date, presence: true
 	validates :end_date, presence: true
+
+  def short_info
+    if self.description.nil?
+      self.project.name
+    else
+      self.project.name + ' (' + self.description + ')'
+    end
+  end
 end
