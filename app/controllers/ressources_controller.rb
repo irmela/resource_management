9d13fd321a65c5badf5875f1ded31ad155d3bb0e
Irmela Goehl <irmela@berlin-coding.de>
2015-10-21 16:14:27 +0200

    making jobs, projects and ressources "readonly" for anonymous users

--- app/controllers/ressources_controller.rb
+++ app/controllers/ressources_controller.rb
@@ -1,4 +1,5 @@
 class RessourcesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_ressource, only: [:show, :edit, :update, :destroy]
 
   # GET /ressources
