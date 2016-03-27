b7e8d4dfdfc66c20400024e08676a1269cc78325
Patrick <patrick@westberliner.net>
2016-03-27 17:57:03 +0200

    adding fixed table header

--- app/assets/javascripts/ressources.js
+++ app/assets/javascripts/ressources.js
@@ -8,4 +8,12 @@ $(document).ready(function() {
   $('#ressource_color').minicolors({
     theme: 'bootstrap'
   });
+  $(document).on('scroll', function(e) {
+    // // create fixed header
+    if($(document).scrollTop() > 108) {
+      $('#calendar table').floatThead({"top":"50px", "autoReflow":true});
+    } else {
+      $('#calendar table').floatThead("destroy");
+    }
+  });
 });
