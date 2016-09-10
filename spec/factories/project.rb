d8b1fab805513d05bc086c8c72d65bc9025726e3
Irmela Goehl <irmela@berlin-coding.de>
2016-09-10 21:06:34 +0200

    adding factories for models job, project and resource

---
+++ spec/factories/project.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :project do
+    name "Test Project 1"
+    department "dev"
+    color "#ff0000"
+  end
+end
---
+++ spec/factories/resource.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :resource do
+    name "John Doe"
+    department "dev"
+    color "#fcfcfc"
+  end
+end
