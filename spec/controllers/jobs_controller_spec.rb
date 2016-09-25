6965c8d1b408914ef0eab3ab2329aa26bddd1487
Irmela Goehl <irmela@berlin-coding.de>
2016-09-25 12:14:35 +0200

    Adding request specs for jobs_controller

---
+++ spec/controllers/jobs_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe JobsController, :type => :controller do
+
+  describe "anonymous access" do
+    describe 'GET #new' do
+      it "requires login" do
+        get :new
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe "POST #create" do
+      it "requires login" do
+        post :create, job: FactoryGirl.attributes_for(:job)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe 'GET #edit' do
+      it "requires login" do
+        get :edit, id: FactoryGirl.create(:job)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe "PATCH #update" do
+      it "requires login" do
+        patch :update, id: FactoryGirl.create(:job), job: FactoryGirl.attributes_for(:job)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe "DELETE #destroy" do
+      it "requires login" do
+        delete :destroy, id: FactoryGirl.create(:job)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    # other examples ...
+  end
+
+end
